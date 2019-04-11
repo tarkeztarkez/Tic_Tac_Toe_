68f0438b0f791297c0be9c5bcfa1405524950c71
tarkeztarkez <44809056+tarkeztarkez@users.noreply.github.com>
2019-04-11 14:45:10 +0200

    Xd

--- lib/Online/OnlineList.dart
+++ lib/Online/OnlineList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+
 class OnlineList extends StatefulWidget {
   OnlineList({Key key}) : super(key: key);
 
